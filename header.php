4273a1e10b4c08f12d91c1cfa0ec8fa08c01b841
FaresElgazzar <fars89231@gmail.com>
2026-09-09 21:52:25 +0300

    home and contact page

---
+++ header.php
@@ -0,0 +1,22 @@
+  <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/all.css"/>
+    <link rel="stylesheet" href="css/libs/animate.css"/>
+ <div class="navbar">
+        <div class="leftnavbar">
+            <h2>Food
+                <span>ORA</span>
+            </h2>
+        </div>
+        <div class="rightnavbar">
+            <ul>
+                <li><a href="home.html">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+        <div class="register">
+            <a href="login.php">Login</a>
+            <a href="register.php">Resgister</a>
+        </div>
+    </div>
